3a9669409c7ead50e87bea85b411c386468fd87d
Prathamesh Chandekar <122272726+pratham1c1@users.noreply.github.com>
2025-12-01 18:46:15 +0530

    Update vite.config.js
    removing the end forward slash

--- PC_Games/vite.config.js
+++ PC_Games/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "/Game_Jam_Version_2/",
+  base : "/Game_Jam_Version_2",
 })
